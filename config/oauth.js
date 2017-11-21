99d6296605f89520832fa70ab53a18df4801c750
john <ayalajohn82@gmail.com>
2017-11-21 11:11:05 -0800

    Add oauth config file.

---
+++ config/oauth.js
@@ -0,0 +1,13 @@
+
+module.exports = {
+  google: {
+    clientID: '--insert-google-client-id-here--',
+    clientSecret: '--insert-google-client-secret-here--',
+    callbackURL: '--insert-google-authorized-redirect-uri-here--',
+  },
+  facebook: {
+    clientID: '--insert-google-client-id-here--',
+    clientSecret: '--insert-google-client-secret-here--',
+    callbackURL: '--insert-google-authorized-redirect-uri-here--',
+  },
+};
