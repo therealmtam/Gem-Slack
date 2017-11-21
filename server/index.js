ff1c1135897228040e357e01f611a850c6863f28
john <ayalajohn82@gmail.com>
2017-11-21 13:08:24 -0800

    passport structure

--- server/index.js
+++ server/index.js
@@ -3,6 +3,11 @@ const express = require('express');
 const path = require('path');
 const socketIO = require('socket.io');
 
+const passport = require('passport');
+const session = require('express-session');
+const GoogleStrategy = require('passport-google-oauth20');
+const authConfig = require('../config/oauth.js');
+
 const app = express();
 
 const server = app.listen(process.env.PORT || 4000, () => {
@@ -62,3 +67,58 @@ socket.on('typing', (data) => {
   feedback.innerHTML = `<p><em>${data} is typing a message...</em></p>`;
 });
 */
+
+app.use(session({
+  secret: 'gemguys',
+  resave: false,
+  saveUninitialized: false,
+}));
+
+app.use(passport.initialize());
+app.use(passport.session());
+
+/**
+ * Simple route middleware to ensure user is authenticated
+ * @param  {} req - request
+ * @param  {} res - response
+ * @param  {} next
+ */
+const ensureAuthenticated = (req, res, next) => {
+  if (req.isAuthenticated()) {
+    next();
+  }
+  res.status(404).send('User not found: incorrect username or password');
+};
+
+passport.serializeUser((user, done) => {
+  // done(null, user.id);
+  done(null, user);
+});
+
+passport.deserializeUser((obj, done) => {
+  // Users.findById(obj, done);
+  done(null, obj);
+});
+
+passport.use(new GoogleStrategy(
+  authConfig.google,
+  (accessToken, refreshToken, profile, done) => done(null, profile),
+));
+
+app.get(
+  '/auth/google',
+  passport.authenticate('google', { scope: ['openid email profile'] }),
+);
+
+app.get(
+  '/auth/google/callback',
+  passport.authenticate('google', { failureRedirect: '/login' }),
+  (req, res) => {
+    // go to correct page
+  },
+);
+
+app.get('/logout', (req, res) => {
+  req.logout();
+  // go back to main page
+});
