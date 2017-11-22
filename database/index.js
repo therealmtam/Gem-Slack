c6de67f27eecd370bfb5270df117772bec200efa
Eric <theericlau@gmail.com>
2017-11-22 13:45:50 -0800

    added local db

--- database/index.js
+++ database/index.js
@@ -1,25 +1,14 @@
 const Sequelize = require('sequelize');
 // const DB = require('../config.js');
 
-//  Parameters are database name, username ,password
-// const sequelize = new Sequelize(`${DB.DATABASE}`, `${DB.USERNAME}`, `${DB.PASSWORD}`, {
-//   host: `${DB.HOST}`,
-//   dialect: 'postgres',
-//   dialectOptions: {
-//     ssl: true,
-//     native: true,
-//   },
-//   pool: {
-//     max: 1000000,
-//     min: 0,
-//     acquire: 30000,
-//     idle: 10000,
-//   },
-// });
-
-const sequelize = new Sequelize(`gem`, `ericlau`, ``, {
-  host: `localhost`,
+ Parameters are database name, username ,password
+const sequelize = new Sequelize(`${DB.DATABASE}`, `${DB.USERNAME}`, `${DB.PASSWORD}`, {
+  host: `${DB.HOST}`,
   dialect: 'postgres',
+  dialectOptions: {
+    ssl: true,
+    native: true,
+  },
   pool: {
     max: 1000000,
     min: 0,
@@ -27,6 +16,17 @@ const sequelize = new Sequelize(`gem`, `ericlau`, ``, {
     idle: 10000,
   },
 });
+
+// const sequelize = new Sequelize(`gem`, `ericlau`, ``, {
+//   host: `localhost`,
+//   dialect: 'postgres',
+//   pool: {
+//     max: 1000000,
+//     min: 0,
+//     acquire: 30000,
+//     idle: 10000,
+//   },
+// });
 
 sequelize
   .authenticate()
