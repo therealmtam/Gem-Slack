4ac01f016e7910e04633d60303b379d93d3aac8f
john <ayalajohn82@gmail.com>
2017-11-22 11:33:53 -0800

    rebase

--- database/index.js
+++ database/index.js
@@ -1,9 +1,25 @@
 const Sequelize = require('sequelize');
-const DB = require('../config.js');
+// const DB = require('../config.js');
 
 //  Parameters are database name, username ,password
+<<<<<<< 9b8e0754753fa9954305631f5d89f25bb6ab7e75
 const sequelize = new Sequelize(`${DB.DATABASE}`, `${DB.USERNAME}`, `${DB.PASSWORD}`, {
   host: `${DB.HOST}`,
+=======
+// const sequelize = new Sequelize(`${DB.DATABASE}`, `${DB.USERNAME}`, `${DB.PASSWORD}`, {
+//   host: `${DB.URI}`,
+//   dialect: 'postgres',
+//   pool: {
+//     max: 1000000,
+//     min: 0,
+//     acquire: 30000,
+//     idle: 10000,
+//   },
+// });
+
+const sequelize = new Sequelize(`slack`, `postgres`, `john123aj`, {
+  host: `localhost`,
+>>>>>>> rebase
   dialect: 'postgres',
   dialectOptions: {
     ssl: true,
