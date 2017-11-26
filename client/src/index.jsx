4af0eacd2e5871602bb884f92e7875aceec1cec8
Max Tam <max.c.tam@gmail.com>
2017-11-26 15:40:51 -0800

    rebase update 9

--- client/src/index.jsx
+++ client/src/index.jsx
@@ -4,7 +4,10 @@ import App from './components/App.jsx';
 
 
 <<<<<<< HEAD
+<<<<<<< HEAD
 
+=======
+>>>>>>> removed sampleData, errorhandled img error newDMview, added socket('connects')
 =======
 >>>>>>> removed sampleData, errorhandled img error newDMview, added socket('connects')
 ReactDOM.render(<App />, document.getElementById('app'));
