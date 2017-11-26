5d53ac8d388ad1ac9dc0e299390b6fcace4b30b2
Max Tam <max.c.tam@gmail.com>
2017-11-26 15:35:00 -0800

    rebase update 5

--- client/src/index.jsx
+++ client/src/index.jsx
@@ -2,39 +2,9 @@ import React from 'react';
 import ReactDOM from 'react-dom';
 import App from './components/App.jsx';
 
-const sampleData = {
-  username: 'theJohn',
-  userImgUrl: 'http://localhost:4000/img3',
-  myRooms: ["therealmtam, theericlau, theJohn, theJeff", "therealmtam, theJeff", "Lobby"],
-  roomMsgs: {
-    "therealmtam, theericlau, theJohn, theJeff": [{
-      username: 'therealmtam',
-      message: "Hello world1!",
-      createdAt: "2017-11-21T19:39:48.279Z",
-      roomname: "therealmtam, theericlau, theJohn, theJeff",
-      userImgUrl: "http://localhost:3000/uploads/userimgs/img3"
-    }],
-    "therealmtam, theJeff": [{
-      username: 'therealmtam',
-      message: "Hello world2!",
-      createdAt: "2017-11-21T19:39:48.279Z",
-      roomname: "therealmtam, theJeff",
-      userImgUrl: "http://localhost:3000/uploads/userimgs/img3"
-    }],
-    "Lobby": [{
-      username: 'therealmtam',
-      message: "Hello world3!",
-      createdAt: "2017-11-21T19:39:48.279Z",
-      roomname: "Lobby",
-      userImgUrl: "https://www.planwallpaper.com/static/images/desktop-year-of-the-tiger-images-wallpaper.jpg"
-    }]
-  },
-    usersInRoom: {
-    "therealmtam, theericlau, theJohn, theJeff": ["therealmtam", "theericlau", "theJohn", "theJeff"],
-    "therealmtam, theJeff": ["therealmtam", "theJeff"],
-    "Lobby": ["therealmtam", "theericlau", "theJohn", "theJeff"]
-  }
-};
 
+<<<<<<< HEAD
 
+=======
+>>>>>>> removed sampleData, errorhandled img error newDMview, added socket('connects')
 ReactDOM.render(<App />, document.getElementById('app'));
