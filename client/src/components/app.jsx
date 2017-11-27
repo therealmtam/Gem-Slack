800836d32cb611b409d8c6c84dd89fe946b1160a
Eric <theericlau@gmail.com>
2017-11-26 19:19:29 -0800

    Update Heroku signin to lowercase

--- client/src/components/app.jsx
+++ client/src/components/app.jsx
@@ -1,6 +1,6 @@
 import React, { Component } from 'react';
 import PropTypes from 'prop-types';
-import SignIn from './Signin.jsx';
+import SignIn from './signin.jsx';
 import Chat from './Chat.jsx';
 import NewDirectMsg from './NewDirectMsg.jsx';
 import axios from 'axios';
