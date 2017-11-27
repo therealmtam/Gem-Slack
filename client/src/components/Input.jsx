f4563eb3afbb2263857a404b5c3f9ed2385ac6f4
Jeffrey Ng <jeffng6688@gmail.com>
2017-11-27 14:52:23 -0800

    Refactor code to stateless components and minor styling

--- client/src/components/Input.jsx
+++ client/src/components/Input.jsx
@@ -1,38 +1,46 @@
-/*eslint-disable */
+
 import React from 'react';
 
 class Input extends React.Component {
-    constructor(props) {
-      super(props);
-        this.state= {
-          message: ''
-        }
-    }
-    handleChange(e) {
+  constructor(props) {
+    super(props);
+    this.state = {
+      message: '',
+    };
+  }
+  handleChange(e) {
+    this.setState({
+      message: e.target.value,
+    });
+  }
+  submitMessage(e) {
+    if (e.charCode === 13 || e.charCode === undefined) {
+      if (this.state.message.length > 0) {
+        this.props.sendMessage(this.state.message);
+      }
       this.setState({
-        message: e.target.value
+        message: '',
       });
     }
-    submitMessage(e) {
-      if (e.charCode === 13 || e.charCode === undefined) {
-        if (this.state.message.length > 0) {
-          this.props.sendMessage(this.state.message);
-        }
-        this.setState({
-          message: ''
-        });
-      }
-    }
-    render() {
-      return(
-        <div className="input-bar">
-          <input className="input-message container-fluid"  
-            placeholder="Message" value={this.state.message} 
-            onChange={this.handleChange.bind(this)} 
-            onKeyPress={this.submitMessage.bind(this)}/>
-        </div>
-        )
-    }
+  }
+  render() {
+    return (
+      <div className="input-bar">
+        <input
+          className="input-message container-fluid"
+          placeholder="Message"
+          value={this.state.message}
+          onChange={this.handleChange.bind(this)}
+          onKeyPress={this.submitMessage.bind(this)}
+        />
+      </div>
+    );
+  }
 }
 
 export default Input;
+
+
+Input.propTypes = {
+};
+
--- client/src/components/MessageList.jsx
+++ client/src/components/MessageList.jsx
@@ -1,41 +1,40 @@
-/*eslint-disable */
 import React from 'react';
 import ReactDOM from 'react-dom';
 import Message from './Message.jsx';
+
 
 class MessageList extends React.Component {
   constructor(props) {
       super(props);
   }
-  scrollToBottom ()  {
-    const node = ReactDOM.findDOMNode(this.messagesEnd);
-    if (Object.keys(this.props.data.roomMsgs).length) {
-      node.scrollIntoView({ behavior: "smooth" });
-    }
-  }
+ /**
+ * scrollToBottom will automatically scroll to bottom of page.
+ * Will scroll on the initial render and after each new message is added.
+ */
   componentDidMount() {
     this.scrollToBottom();
   }
   componentDidUpdate() {
     this.scrollToBottom();
   }
+  scrollToBottom() {
+    const node = ReactDOM.findDOMNode(this.messagesEnd);
+    if (Object.keys(this.props.data.roomMsgs).length) {
+      node.scrollIntoView({ behavior: 'smooth' });
+    }
+  }
   render() {
     if (Object.keys(this.props.data.roomMsgs).length) {
       return (
         <ul className="message-list"> 
-          { 
-            this.props.data.roomMsgs[this.props.data.currentRoom].map((msg, index) => {
-              return <Message msg={msg} key={index}/>
+          { this.props.data.roomMsgs[this.props.data.currentRoom].map((msg, index) => {
+              return <Message msg={msg} key={index} />;
             })
           }
-          <div style={{ float:"left", clear: "both" }}
-              ref={(el) => { this.messagesEnd = el; }}>
-          </div>
+          <div style={{ float: 'left', clear: 'both' }} ref={(el) => { this.messagesEnd = el; }} />
         </ul>
-      )
-    } else {
-      return <ul className="message-list"></ul>
-    }
+      );
+    } return <ul className="message-list" />;
   }
 }
 
