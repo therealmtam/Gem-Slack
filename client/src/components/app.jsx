44ebb997eda55ad9d186c8cd554ae86aae2af58c
Eric <theericlau@gmail.com>
2017-11-27 09:12:04 -0800

    Heroku change

--- client/src/components/app.jsx
+++ client/src/components/app.jsx
@@ -6,7 +6,7 @@ import NewDirectMsg from './NewDirectMsg.jsx';
 import axios from 'axios';
 import io from 'socket.io-client';
 
-const socket = io('http://localhost:4000');
+const socket = io('process.env.PORT');
 
 /**
  * Description:
