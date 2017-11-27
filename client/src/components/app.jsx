642ee7acaeb14fe0a817026bde9bc06c8c8c9da3
Eric <theericlau@gmail.com>
2017-11-27 10:03:48 -0800

    rebase

--- client/src/components/app.jsx
+++ client/src/components/app.jsx
@@ -111,8 +111,8 @@ class App extends Component {
    * @param {String} username - Username typed in by the user
    */
   sendUserNameToServer(username, imageUrl) {
-    let image = imageUrl ? imageUrl :
-    'https://typeset-beta.imgix.net/rehost%2F2016%2F9%2F13%2F3538f891-a083-4310-a419-84e8c709a635.jpg'
+    const image = imageUrl ? imageUrl :
+      'https://typeset-beta.imgix.net/rehost%2F2016%2F9%2F13%2F3538f891-a083-4310-a419-84e8c709a635.jpg';
 
     socket.emit('user login', {
       username: username,
