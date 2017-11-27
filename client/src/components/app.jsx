361b086ad694ff87f1eda235d9b82e77fc5b34eb
Eric <theericlau@gmail.com>
2017-11-27 11:05:12 -0800

    Heroku update

--- client/src/components/app.jsx
+++ client/src/components/app.jsx
@@ -7,7 +7,8 @@ import axios from 'axios';
 import io from 'socket.io-client/dist/socket.io';
 //  Todo: Update socket-io client after fix of webpack issue
 
-const socket = io(`${process.env.PORT}`);
+const socket = io(process.env.PORT);
+console.log('im the port for heroku', process.env.PORT);
 // const socket = io('http://localhost:4000');
 
 /**
